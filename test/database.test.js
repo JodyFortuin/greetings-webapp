2836e14c6be9a928cfa1b9644bfe3b3f47ca3c6b
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-09-07 14:02:51 +0000

    Add files via upload

---
+++ test/database.test.js
@@ -0,0 +1,65 @@
+let assert = require("assert");
+let greetFactory = require("../greet");
+
+describe("Greet Database Unit Test", function() {
+
+const pg = require("pg");
+const Pool = pg.Pool;
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/greetings';
+const pool = new Pool({
+	connectionString
+});
+
+const INSERT_QUERY = "insert into users (name, greet_count) values ($1, $2)";
+
+beforeEach(async function () {
+	await pool.query("delete from users");
+});
+
+
+  it("should greet name entered (Kyle)", async function() {
+
+    let greetName = greetFactory(pool);
+
+    assert.equal("Hello, Kyle", greetName.language('Kyle', 'English'));
+});
+
+  it("should greet in the language chosen (English)", async function() {
+
+    let greetName = greetFactory(pool);
+
+    assert.equal('Hello, Jack', greetName.language('Jack', 'English'));
+  });
+
+
+  it("should greet in the language chosen (Afrikaans)", async function() {
+
+    let greetName = greetFactory(pool);
+
+    assert.equal('Halo, James', greetName.language('James', 'Afrikaans'));
+  });
+
+
+  it("should greet in the language chosen (isiXhosa)", async function() {
+
+    let greetName = greetFactory(pool);
+
+    assert.equal('Molo, Julien', greetName.language('Julien', 'isiXhosa'));
+
+  });
+
+
+  it("should return the count of names greeted (3)", async function() {
+
+    let greetName = greetFactory(pool);
+
+    await greetName.addMap('Jody');
+    await greetName.addMap('Julien');
+    await greetName.addMap('James');
+
+    assert.equal(3, greetName.countNames());
+
+
+  });
+
+});
